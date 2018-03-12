0bd599371cffeaf105467713b5aca4f0ee65603f
Kaila Santiago <ksantiago.student@hsfi.us>
2018-03-12 01:09:47 +0000

    started to work on display list

--- js/script.js
+++ js/script.js
@@ -1,62 +1,58 @@
 // quick fix for Cloud9 warning:
-/* global $ */
+/* global $ myPlaylist*/
+var songInput = $("#songInput").val();
+var artistInput = $("#artistInput").val();
+var mp3Url = $("#mp3-url").val();
+var imageUrl = $("#image-url").val();
 
-// Class 2:
-// Complete displayList() to show a single song in the list
-
-// Class 2:
-// Complete displayList() to show all the songs in the list
-// Complete clearList() funcion
-
-// Class 3:
-// Complete the addSong function to take an input from input boxes and push a new song to the playlist array. 
-
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-// Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
-	"song-length":"3 minutes",
+	"song":"songInput",
+	"aritst":"artistInput",
+	"mp3url":"mp3Url",
+	"imageURL":"imageURL"
 }
 
 var myPlayList = [
 	{
 		"title":"24K Magic",
 		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"mp3url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"imageURL":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
 		"title":"Sir Duke",
 		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"mp3url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"imageURL":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
 		"title":"Sorry",
 		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"mp3url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"imageURL":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
 ]
 
-
-
-// DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-
+	displayList(myPlayList);
 });
 
 function displayList(){
-	
+	var title = myPlaylist[0].title;
+	var artist = myPlaylist[0].artist;
+	var mp3URL = myPlaylist[0].mp3URL;
+	var imageURL = myPlaylist[0].imageURL;
+	for (var i=0; i < myPlaylist.length; i++) {
+		$("body").append(`
+			<figure class=”frame”>
+				<img src=${imageURL}>
+				<figcaption>${title} by ${artist}</figcaption>
+			</figure>
+		`)
+	}
 }
-
+	
 function clearList(){
   
   
@@ -64,5 +60,6 @@ function clearList(){
 }
 
 function addSong(){
- 
+	myPlaylist.push("");
+
 }
